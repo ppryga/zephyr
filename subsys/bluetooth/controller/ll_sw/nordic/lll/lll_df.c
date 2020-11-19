c859d3351a267b5444418c5219a666c5c576cb3c
Piotr Pryga <piotr.pryga@nordicsemi.no>
2020-11-19 02:12:54 -0800

    Bluetooth: controller: Add DF initialization to controller init sequence
    Direction Finding requires initalization of set of registers
    in Radio peripheral. Also it requires information about antenna
    matrix unit that are provided by end user in DTS overlay.
    Implemented initialization step is responsible for validation of
    hardware information delivered by DTS and setting up DF related
    registers in Radio.
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_df.c
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_df.c
@@ -8,6 +8,49 @@
 #include "hal/radio_df.h"
 #include "lll_df.h"
 
+/* @brief Function performs common steps for initialization and reset
+ * of Direction Finding LLL module.
+ *
+ * @return	Zero in case of success, other value in case of failure.
+ */
+static int init_reset(void);
+
+int lll_df_init(void)
+{
+	int err;
+
+	err = init_reset();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+int lll_df_reset(void)
+{
+	int err;
+
+	err = init_reset();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+static int init_reset(void)
+{
+	int err;
+
+	err = radio_df_ant_configure();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
 uint8_t lll_df_ant_num_get(void)
 {
 	return radio_df_ant_num_get();
