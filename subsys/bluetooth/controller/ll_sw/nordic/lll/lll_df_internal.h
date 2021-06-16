27afaf16c20997945ed3fa3b313f96546202785d
Piotr Pryga <piotr.pryga@nordicsemi.no>
2021-06-16 11:30:56 +0200

    Bluetooth: controller: LLL: Add CTE confing for per. adv. chain TX
    Add configuration of CTE for periodic advertising chain transmission.
    The commit provides configuration of radio in prepare stage and
    in handle of Tx ISR. CTE is configured only for PDUs that
    have cte_info field in extended advertising header.
    
    During prepare of periodic advertising event there are updated
    aux_ptr fields in extended advertising header in other PDUs from
    a periodic advertising chain. aux_ptr offset value also depends
    on CTE length. CTE configuration is always the same for every
    PDU in periodic advertising chain.
    CTE may be added to requested number of PDUs in periodic
    advertising chain. Although it is possible that there are PDUs
    that don't have CTE. PDUs that have CTE are alsways at the beginning
    of a periodic advertising chain.
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_df_internal.h
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_df_internal.h
@@ -5,8 +5,8 @@
  */
 
 /* Enables CTE transmission according to provided configuration */
-void lll_df_conf_cte_tx_enable(uint8_t type, uint8_t length,
-			       uint8_t ant_num, uint8_t *ant_ids);
+void lll_df_cte_tx_enable(struct lll_adv_sync *lll_sync, const struct pdu_adv *pdu,
+			  uint32_t *cte_len_us);
 /* Disables CTE transmission */
 void lll_df_conf_cte_tx_disable(void);
 
